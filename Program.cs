38da9ddefd70f305dffa2d1cfed7f8ab3e8ae030
Cassiano-Behemonth <oliveira.cassiano@escola.pr.gov.br>
2025-05-01 02:15:10 -0300

    Conexão de banco sqlite

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using MercadoAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=mercado.db"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "API de Estoque de Mercado - funcionando!");
+
+
 
 app.Run();
