60fe2e81473023a50fd77ad1087afe799e4e7c52
Samuel Nogueira <146238530+SamuelN-L@users.noreply.github.com>
2025-05-01 02:34:53 -0300

    adicionando swagger

--- Program.cs
+++ Program.cs
@@ -5,15 +5,20 @@ using MercadoAPI.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();          
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=mercado.db"));
 
 var app = builder.Build();
 
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.MapGet("/", () => "API de Estoque de Mercado - funcionando!");
-
 app.MapCategoriaEndpoints();
-
 app.MapProdutoEndpoints();
 
 app.Run();
