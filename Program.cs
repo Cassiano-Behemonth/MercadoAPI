914f856aec7cfc762c0b6e961cbac6edd187b591
Samuel Nogueira <146238530+SamuelN-L@users.noreply.github.com>
2025-06-13 20:08:20 -0300

    adicionado o cors

--- Program.cs
+++ Program.cs
@@ -4,15 +4,23 @@ using MercadoAPI.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();          
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=mercado.db"));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
 var app = builder.Build();
 
+app.UseCors("AllowAll");
 
 app.UseSwagger();
 app.UseSwaggerUI();
